3decd94fb52d7d04c4cd6d67422fe2584fc56c71
Marjan Fariborz <mfariborz@ucdavis.edu>
2021-09-23 16:45:40 -0700

    Adding SSSP algorithm

--- GAcc.py
+++ GAcc.py
@@ -2,6 +2,7 @@ from math import inf
 import numpy as np
 from scipy.sparse import csr_matrix
 import multiprocessing
+import numpy as np
 
 def GraphStructure(graphFile):
     Graph = []
@@ -17,41 +18,62 @@ def GraphStructure(graphFile):
     gr = np.array(Graph, dtype=int)
     el = np.array(EdgeList, dtype=int)
     num_v = np.amax(el)
-    print(f"Max number of vertecies:{num_v} ")
+    print(f"Max number of vertecies:{num_v}")
     rows, cols, vals = zip(*Graph)
     a = csr_matrix((vals, (rows, cols)))
     prop = [inf]*(num_v+1)
     return a, prop, num_v+1
 
-Graph, vertex_prop, num_v = GraphStructure('USA-road-d.NY.gr')
-Graph_arr = Graph.toarray()
-print(Graph_arr)
-keys = [i for i in range(num_v)]
-event_queue = {key: None for key in keys}
-event_queue[0] = 0
-itr = int(0)
-n = 0
-while(1):
-    if (all(value == None for value in event_queue.values())):
-        break
-    n += 1
-    for key, value in event_queue.items():
-        if value != None:
-            temp = vertex_prop[key]
-            vertex_prop[key] = min(vertex_prop[key], value)
+def degree_count(graph):
+    Graph, vertex_prop, num_v = GraphStructure(graph)
+    file = open('degree_count.csv', 'w')
+    Graph_arr = Graph.toarray()
+    degree_list = []
+    for row in Graph_arr:
+        degree = 0
+        for index in row:
+            if index != 0:
+                degree += 1
+        degree_list.append(degree)
+        file.write(str(degree))
+        file.write('\n')
 
-            if temp != 0:
-                itr += 1
-                # print(itr)
-                row = Graph_arr[key]
-                for i,val in enumerate(row):
-                    if val != 0:
-                        event_queue[i]=0
-                event_queue[key] = None
-            else:
-                event_queue[key] = None
+def action(algorithm):
+    if algorithm == 'BFS':
+        return min, min
+    elif algorithm == 'SSSP':
+        return min, sum
 
-for i, k in enumerate(vertex_prop):
-    if k == inf:
-        print(i)
-# print(vertex_prop)
+def main(algorithm, graph):
+    Graph, vertex_prop, num_v = GraphStructure(graph)
+    Graph_arr = Graph.toarray()
+    print(Graph_arr)
+    keys = [i for i in range(num_v)]
+    event_queue = {key: None for key in keys}
+    event_queue[0] = 0
+    itr = int(0)
+    n = 0
+    reduce, propagate = action(algorithm)
+    while(1):
+        if (all(value == None for value in event_queue.values())):
+            break
+        n += 1
+        for vertex, paylaod in event_queue.items():
+            if paylaod != None:
+                temp = vertex_prop[vertex]
+                vertex_prop[vertex] = reduce(vertex_prop[vertex], paylaod)
+                if 0 < abs(temp - vertex_prop[vertex]):
+                    itr += 1
+                    row = Graph_arr[vertex]
+                    for i,val in enumerate(row):
+                        if val != 0:
+                            if event_queue[i] != None:
+                                event_queue[i]= reduce(propagate([0, val]), event_queue[i])
+                            else:
+                                event_queue[i]= propagate([0, val])
+                    event_queue[vertex] = None
+                else:
+                    event_queue[vertex] = None
+
+main('BFS', 'USA-road-d.NY.gr')
+
