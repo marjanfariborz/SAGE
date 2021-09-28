e9ddc18d24626e4f95629de71c84a9c753a8b41b
Mahyar Samani <msamani@ucdavis.edu>
2021-09-28 16:22:43 -0700

    Adding a function to convert txt to json and a function to convert txt to a list.

---
+++ graph_reader.py
@@ -0,0 +1,45 @@
+import re
+import json
+
+from json.encoder import JSONEncoder
+
+class Vertex():
+    def __init__(self, vid, dst = None):
+        self.id = vid
+        self.neighbors = []
+        if dst is not None:
+            self.neighbors.append(dst)
+
+    def add_neighbor(self, dst):
+        self.neighbors.append(dst)
+
+class VertexEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+def read_graph(file_name):
+    vertices = {}
+
+    with open(file_name, "r") as graph:
+        for line in graph.readlines():
+            if not line.startswith("#"):
+                src, dst = line.split()
+                if src in vertices.keys():
+                    vertices[src].add_neighbor(dst)
+                else:
+                    vertices[src] = Vertex(src, dst)
+
+    vertex_list = []
+    for _, value in vertices.items():
+        vertex_list.append(value)
+
+    return vertex_list
+
+def graph_to_json(file_name):
+    vertex_list = read_graph(file_name)
+    json_name = re.sub(r"\..*", ".json", file_name)
+    with open(json_name, "w") as json_file:
+        json.dump(vertex_list, json_file, indent = 2, cls = VertexEncoder)
+
+if __name__ == "__main__":
+    graph_to_json("roadNet-CA.txt")
