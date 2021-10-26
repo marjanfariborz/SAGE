acc6f61754851d935b973af26cb88984a7faa98e
Marjan Fariborz <mfariborz@ucdavis.edu>
2021-10-26 12:49:48 -0700

    [working] reduce and apply

--- mpu.py
+++ mpu.py
@@ -1,10 +1,10 @@
-from reduce_wl import ReduceWL
+from wl_engine import WLEngine
 from memory_device import MemoryDevice
 
 class MPU():
     def __init__(self, mid, operation):
         self.id = mid
-        self.reduce_wl = ReduceWL(self, operation)
+        self.wl_engine = WLEngine(self, operation)
         self.mem_dev = MemoryDevice(self)
 
     def read_work_list_item(self, vid):
@@ -15,4 +15,10 @@ class MPU():
 
     def write_edge_list(self, edge_list):
         self.mem_dev.write_edge_list(edge_list)
+
+    def read_edge_list(self, vid):
+        self.mem_dev.read_work_list_item(vid)
+
+    def get_id(self):
+        return self.id
 
--- reduceApply.py
+++ reduceApply.py
@@ -1,16 +1,28 @@
 from graph_reader import Vertex
 
 class Apply():
-    def __init__(self, id, op = sum):
-        self.id = id
+    def __init__(self, owner, op = sum):
+        self.owner = owner
         self.op = op
+        self.vid = 0
+
     def arbitrate(self):
+        work_list_item = self.owner.read_work_list_item(self.vid)
+        if work_list_item.is_valid():
+            temp_prop = work_list_item.get_temp_prop()
+            prop = work_list_item.get_prop()
+            prop_n = self.op(temp_prop, prop)
+            work_list_item.invalidate()
+            if prop_n != prop:
+                work_list_item.set_prop(prop_n)
+                # TODO: Add reading the edgelist and push here
+        # TODO: add termination condition here
+        self.vid = self.vid +1
+        self.arbitarte()
+
         '''
         arbitarte in its local worklist
         finds a vid in worklist with valid =1
         self.vtemp = vtemp
         self.value = value
         '''
-
-    def reduce(self):
-        op = self.op
