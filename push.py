cd39ab89977e16ea7f94f119be87c2b876ddf54d
Marjan Fariborz <mfariborz@ucdavis.edu>
2021-10-29 10:35:33 -0700

    [WIP] Push, cleaning up mpu and memory device

--- push.py
+++ push.py
@@ -1,41 +1,27 @@
 
+
 class Push():
-    def __init__(self, id, op = sum):
-        self.op = op
+    def __init__(self, owner, operation):
+        self.owner = owner
+        self.operation = operation
         self.localQ = []
         self.globalQ = []
 
-    def recv_update(self, vid, value, weight):
-        '''
-        interface to reduce
-        '''
-        self.value = value
-        self.weight = weight
-        self.vid = vid
+    def recv_update(self, edges, temp_prop):
+        self.owner.read_apply()
+        self.value = temp_prop
+        self.weight = edges.get_weight()
+        self.vid = edges.get_neighbor()
         self.push(self)
 
     def send_update(self, vid, value):
-        '''
-        based on the vid it either sends the futur work
-        locally or globally
-        I think it needs to be seperated
-
-        if local:
-            self.localQ.pop(0)
-        else:
-            self.globalQ.pop(0)
-        '''
-        if (vid % 2) == id:
+        if (vid % 2) == self.owner.get_id():
             self.localQ.append({vid, value})
         else:
             self.globalQ.append({vid, value})
 
     def push(self):
-        '''
-        Performs push operation and calls send update and based on
-        vid they need to store in local/globalQ
-        '''
-        propagate = self.op
+        propagate = self.operation
         new_value = propagate(self.value, self.weight)
         self.send_update(self.vid, new_value)
 
